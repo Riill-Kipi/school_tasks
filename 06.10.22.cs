652aea88b5e3376815c06e953dc8458f4cf27754
Riill-Kipi <69919163+Riill-Kipi@users.noreply.github.com>
2022-11-01 23:38:49 +0500

    Add files via upload

---
+++ 06.10.22.cs
@@ -0,0 +1,70 @@
+﻿//13
+//string a = Console.ReadLine();
+//for (int i = 0; i < a.Length; i++)
+//{
+//    Console.Write(a[i] + " ");
+//}
+//for (int i = 0; i < a.Length; i++)
+//{
+//    if (i < a.Length - 1)
+//    {
+//        Console.Write(a[i]);
+//        Console.WriteLine(a[i + 1] + " ");
+//    }
+
+//}
+//Console.WriteLine();
+//for (int i = 0; i < 1; i++)
+//{
+//    Console.Write(a[i]);
+//    Console.WriteLine(a.Substring(a.Length - 1));
+//    Console.WriteLine(" " + a);
+//}
+
+
+06
+int[] array = new int[10];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(0, 3);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+}
+Console.WriteLine();
+Console.WriteLine("Какое число хотите удалить?");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == n)
+    {
+        Console.Write("");
+    }
+    else
+    {
+        Console.Write(array[i]);
+    }
+}
+Console.WriteLine("");
+foreach (int i in array)
+{
+    if (i == n)
+    {
+        continue;
+    }
+    Console.Write(i);
+}
+Console.WriteLine("");
+int m = 0;
+while (m < array.Length)
+{
+    if (array[m] == n)
+    {
+        Console.Write("");
+    }
+    else
+    {
+        Console.Write(array[m]);
+    }
+    m++;
+}
---
+++ 07_10_22.cs
@@ -0,0 +1,94 @@
+﻿// №1
+string a = "GGETRA";
+int b = a.Length;
+if (b % 2 == 1)
+{
+    int n = b / 2;
+    Console.WriteLine(a[n]);
+}
+else if (b % 2 == 0)
+{
+    int v = b / 2 - 1;
+    Console.Write(a[v]);
+    v = v + 1;
+    Console.WriteLine(a[v]);
+}
+
+// №2
+
+int[,] b = { { 14, 14 }, { 42, 20 }, { 61, 1 }, { 37, 8 }, { 21, 21 }, { 78, 9 } };
+string[] c = new string[6];
+
+for (int i = 0; i < b.GetLength(0); i++)
+{
+    if (b[i, 0] > 36 && b[i, 1] > 6)
+    {
+        c[i] = "Senior";
+    }
+    else
+    {
+        c[i] = "Player";
+    }
+}
+
+Console.WriteLine(" ");
+for (int i = 0; i < c.Length; i++)
+{
+    Console.Write(" " + c[i] + ",");
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] mas = new int[a, b];
+if (a > 36 & b > 6)
+{
+    Console.WriteLine("senjor");
+}
+else
+{
+    Console.WriteLine("player");
+}
+
+ //№3
+
+namespace Nomer_3
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string c = "hg73543lhli83524jgy8465";
+            char[] a = c.ToCharArray();
+            Console.WriteLine(a);
+            string eet = "";
+            int q = c.Length;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == '8')
+                {
+                    char[] ar;
+                    int n = q - i;
+                    string txt = Convert.ToString(i);
+                    int v = Convert.ToInt32(txt);
+                    ar = c.ToCharArray(v, n);
+                    for (i = 0; i < ar.Length; i++)
+                    {
+                        if (ar[i] == '1' || ar[i] == '2' || ar[i] == '3' || ar[i] == '4' || ar[i] == '5' || ar[i] == '6' || ar[i] == '7' || ar[i] == '8' || ar[i] == '9')
+                        {
+                            eet = eet + Convert.ToString(ar[i]);
+                        }
+
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine(eet);
+        }
+
+
+
+    }
+}
